b8779e914859832fdfe3943c58be6d0892c384d4
yogitashete2412 <yogitashete24@gmail.com>
2023-07-21 12:20:44 +0530

    feat: home content added

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">ButtonXpert</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    </div>
+  </div>
+</nav>
+    </>
+
+  )
+}
+
+export default Navbar
