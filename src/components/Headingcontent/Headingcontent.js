a3be4c561198bb922b84387915ccdef1375f33cd
yogitashete2412 <yogitashete24@gmail.com>
2023-07-21 13:31:18 +0530

    ref : component name changed

--- src/components/Headingcontent/Headingcontent.js
+++ src/components/Headingcontent/Headingcontent.js
@@ -9,6 +9,13 @@ function Headingcontent() {
     <div className='main-container'>
 
     <h1 className='text-center project-heading mt-5'>ButtonXpert</h1>
+
+    <h3 className='mt-5 description text-center'>Click Here, To Use Beautiful Buttons</h3>
+
+
+    <div class="d-grid gap-2 col-2 mx-auto">
+  <button className="btn btn-dark mt-5 main-button" type="button">Show More</button>
+</div>
     
     </div>
     
--- src/components/Tavbar/Tavbar.js
+++ src/components/Tavbar/Tavbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './Navbar.css'
+import './Tavbar.css'
 
-function Navbar() {
+function Tavbar() {
   return (
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">ButtonXpert</a>
+    <a className="navbar-brand btn-heading" href="#">ButtonXpert</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -19,4 +19,4 @@ function Navbar() {
   )
 }
 
-export default Navbar
+export default Tavbar
