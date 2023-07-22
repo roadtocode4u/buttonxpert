32ea323f78a9b64a547ac64adfae4278d856c493
Harshda Mhase <harshadamhase12@gmail.com>
2023-07-21 20:24:30 -0900

    ref: 3d button design added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Tavbar from './components/Tavbar/Tavbar';
+import Navbar from './components/Navbar/Navbar'
 import Headingcontent from './components/Headingcontent/Headingcontent';
-
+import ButtonPage from "./ButtonPage";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
-    <>
+      <>
+      
     <Navbar/>
     <Headingcontent/>
+   <ButtonPage/>
+   
     </>
-  )
+    );
 }
 
 export default App;
