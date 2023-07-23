051f073ac71f80c45633817e48e5762259d21764
yogitashete2412 <yogitashete24@gmail.com>
2023-07-23 13:50:28 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Tavbar from './components/Tavbar/Tavbar';
-import Headingcontent from './components/Headingcontent/Headingcontent';
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-    <Tavbar/>
-    <Headingcontent/>
+   
     </>
   )
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Headingcontent from './components/Headingcontent/Headingcontent';
+import Navbar from "./components/Navbar/Navbar";
+
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Headingcontent/>
+    </>
+  )
+}
+
+export default Home
